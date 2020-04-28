133ba672e2e08541feed3124983ddb0deae85450
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-04-28 14:45:43 -0500

    Removed testing data.  Ran linter.

--- routes/api-data-routes.js
+++ routes/api-data-routes.js
@@ -1,231 +1,3 @@
-const results = JSON.parse(`
-{"results": [
-    {
-    "id": "5d914163302b840050ad1c67",
-    "picture": "https://utellyassets9-1.imgix.net/api/Images/25555312ae6905f37e0ded6757a1d3a4/Redirect",
-    "name": "Parasyte: The Maxim",
-    "locations": [
-    {
-    "icon": "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/iTunesIVAUS.png?w=92&auto=compress&app_version=ae3576e2-0796-4eda-b953-80cadc8e2619_eww2020-04-28",
-    "display_name": "iTunes",
-    "name": "iTunesIVAUS",
-    "id": "5d80a9a5d51bef861d3740d3",
-    "url": "https://itunes.apple.com/us/tv-season/what-mad-universe/id1161431598?i=1163455431"
-    },
-    {
-    "icon": "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/AmazonInstantVideoIVAUS.png?w=92&auto=compress&app_version=ae3576e2-0796-4eda-b953-80cadc8e2619_eww2020-04-28",
-    "display_name": "Amazon Instant Video",
-    "name": "AmazonInstantVideoIVAUS",
-    "id": "5d82609332ac2f0051962fe6",
-    "url": "https://www.amazon.com/gp/product/B01A4Q46UO?creativeASIN=B01A4Q46UO&ie=UTF8&linkCode=xm2&tag=utellycom00-21"
-    },
-    {
-    "icon": "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/HuluIVAUS.png?w=92&auto=compress&app_version=ae3576e2-0796-4eda-b953-80cadc8e2619_eww2020-04-28",
-    "display_name": "Hulu",
-    "name": "HuluIVAUS",
-    "id": "5d824d7e0579d00051b0d33c",
-    "url": "https://www.hulu.com/watch/15670bcf-b23e-4e89-bbc0-50f8973d1885"
-    }
-    ],
-    "provider": "iva",
-    "weight": 5984,
-    "external_ids": {
-    "iva_rating": null,
-    "imdb": {
-    "url": "https://www.imdb.com/title/tt3358020",
-    "id": "tt3358020"
-    },
-    "tmdb": {
-    "url": "https://www.themoviedb.org/movie/61459",
-    "id": "61459"
-    },
-    "wiki_data": null,
-    "iva": {
-    "id": "722542"
-    },
-    "gracenote": null,
-    "rotten_tomatoes": null,
-    "facebook": null
-    }
-    },
-    {
-    "id": "5d97da419a76a40056de28cc",
-    "picture": "https://utellyassets9-1.imgix.net/api/Images/57e29183a59ac652fdef9d34d72d82aa/Redirect",
-    "name": "Parasite",
-    "locations": [
-    {
-    "icon": "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/AmazonPrimeVideoIVAUS.png?w=92&auto=compress&app_version=ae3576e2-0796-4eda-b953-80cadc8e2619_eww2020-04-28",
-    "display_name": "Amazon Prime Video",
-    "name": "AmazonPrimeVideoIVAUS",
-    "id": "5d8257243ed3f000513540ec",
-    "url": "https://www.amazon.com/gp/product/B076XWZ33H?creativeASIN=B076XWZ33H&ie=UTF8&linkCode=xm2&tag=utellycom00-21"
-    },
-    {
-    "icon": "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/AmazonInstantVideoIVAUS.png?w=92&auto=compress&app_version=ae3576e2-0796-4eda-b953-80cadc8e2619_eww2020-04-28",
-    "display_name": "Amazon Instant Video",
-    "name": "AmazonInstantVideoIVAUS",
-    "id": "5d82609332ac2f0051962fe6",
-    "url": "https://www.amazon.com/gp/product/B076XWZ33H?creativeASIN=B076XWZ33H&ie=UTF8&linkCode=xm2&tag=utellycom00-21"
-    }
-    ],
-    "provider": "iva",
-    "weight": 0,
-    "external_ids": {
-    "iva_rating": null,
-    "imdb": {
-    "url": "https://www.imdb.com/title/tt0084472",
-    "id": "tt0084472"
-    },
-    "tmdb": {
-    "url": "https://www.themoviedb.org/movie/48311",
-    "id": "48311"
-    },
-    "wiki_data": {
-    "url": "https://www.wikidata.org/wiki/Q1194793",
-    "id": "Q1194793"
-    },
-    "iva": {
-    "id": "467404"
-    },
-    "gracenote": null,
-    "rotten_tomatoes": null,
-    "facebook": null
-    }
-    },
-    {
-    "id": "5e2ce0b290c0e033a487fbf6",
-    "picture": "https://utellyassets9-1.imgix.net/api/Images/51831fbdf0d6ae6e155a448619221538/Redirect",
-    "name": "Parasite",
-    "locations": [
-    {
-    "icon": "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/AmazonInstantVideoIVAUS.png?w=92&auto=compress&app_version=ae3576e2-0796-4eda-b953-80cadc8e2619_eww2020-04-28",
-    "display_name": "Amazon Instant Video",
-    "name": "AmazonInstantVideoIVAUS",
-    "id": "5d82609332ac2f0051962fe6",
-    "url": "https://www.amazon.com/gp/product/B07YM24L3B?creativeASIN=B07YM24L3B&ie=UTF8&linkCode=xm2&tag=utellycom00-21"
-    },
-    {
-    "icon": "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/iTunesIVAUS.png?w=92&auto=compress&app_version=ae3576e2-0796-4eda-b953-80cadc8e2619_eww2020-04-28",
-    "display_name": "iTunes",
-    "name": "iTunesIVAUS",
-    "id": "5d80a9a5d51bef861d3740d3",
-    "url": "https://itunes.apple.com/us/movie/parasite/id1482083457"
-    },
-    {
-    "icon": "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/GooglePlayIVAUS.png?w=92&auto=compress&app_version=ae3576e2-0796-4eda-b953-80cadc8e2619_eww2020-04-28",
-    "display_name": "Google Play",
-    "name": "GooglePlayIVAUS",
-    "id": "5d8260b128fbcd0052aed197",
-    "url": "https://play.google.com/store/movies/details/Parasite?gl=us&hl=en&id=oiF1dHVcqM8"
-    },
-    {
-    "icon": "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/HuluIVAUS.png?w=92&auto=compress&app_version=ae3576e2-0796-4eda-b953-80cadc8e2619_eww2020-04-28",
-    "display_name": "Hulu",
-    "name": "HuluIVAUS",
-    "id": "5d824d7e0579d00051b0d33c",
-    "url": "https://www.hulu.com/watch/2fd691a0-f66b-467f-8635-00d7f151f3d4"
-    }
-    ],
-    "provider": "iva",
-    "weight": 0,
-    "external_ids": {
-    "iva_rating": null,
-    "imdb": {
-    "url": "https://www.imdb.com/title/tt6751668",
-    "id": "tt6751668"
-    },
-    "tmdb": {
-    "url": "https://www.themoviedb.org/movie/496243",
-    "id": "496243"
-    },
-    "wiki_data": {
-    "url": "https://www.wikidata.org/wiki/Q61448040",
-    "id": "Q61448040"
-    },
-    "iva": {
-    "id": "372365"
-    },
-    "gracenote": null,
-    "rotten_tomatoes": null,
-    "facebook": null
-    }
-    },
-    {
-    "id": "5e2ce0ef90c0e033a4881dbd",
-    "picture": "https://utellyassets9-1.imgix.net/api/Images/841c92ad481e3c37de326a4b8ad053b4/Redirect",
-    "name": "The Parasite Doctor Suzune: Evolution",
-    "locations": [
-    {
-    "icon": "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/AmazonInstantVideoIVAUS.png?w=92&auto=compress&app_version=ae3576e2-0796-4eda-b953-80cadc8e2619_eww2020-04-28",
-    "display_name": "Amazon Instant Video",
-    "name": "AmazonInstantVideoIVAUS",
-    "id": "5d82609332ac2f0051962fe6",
-    "url": "https://www.amazon.com/gp/product/B00JZH02CU?creativeASIN=B00JZH02CU&ie=UTF8&linkCode=xm2&tag=utellycom00-21"
-    }
-    ],
-    "provider": "iva",
-    "weight": 0,
-    "external_ids": {
-    "iva_rating": null,
-    "imdb": {
-    "url": "https://www.imdb.com/title/tt2100558",
-    "id": "tt2100558"
-    },
-    "tmdb": {
-    "url": "https://www.themoviedb.org/movie/252944",
-    "id": "252944"
-    },
-    "wiki_data": null,
-    "iva": null,
-    "gracenote": null,
-    "rotten_tomatoes": null,
-    "facebook": null
-    }
-    },
-    {
-    "id": "5e2ce0ef90c0e033a4881dbe",
-    "picture": "https://utellyassets9-1.imgix.net/api/Images/c53638d6f90986200c40d4f7274cb41e/Redirect",
-    "name": "The Parasite Doctor Suzune: Genesis",
-    "locations": [
-    {
-    "icon": "https://utellyassets7.imgix.net/locations_icons/utelly/black_new/AmazonInstantVideoIVAUS.png?w=92&auto=compress&app_version=ae3576e2-0796-4eda-b953-80cadc8e2619_eww2020-04-28",
-    "display_name": "Amazon Instant Video",
-    "name": "AmazonInstantVideoIVAUS",
-    "id": "5d82609332ac2f0051962fe6",
-    "url": "https://www.amazon.com/gp/product/B00GJYT9E4?creativeASIN=B00GJYT9E4&ie=UTF8&linkCode=xm2&tag=utellycom00-21"
-    }
-    ],
-    "provider": "iva",
-    "weight": 0,
-    "external_ids": {
-    "iva_rating": null,
-    "imdb": {
-    "url": "https://www.imdb.com/title/tt2047769",
-    "id": "tt2047769"
-    },
-    "tmdb": {
-    "url": "https://www.themoviedb.org/movie/260015",
-    "id": "260015"
-    },
-    "wiki_data": {
-    "url": "https://www.wikidata.org/wiki/Q6416448",
-    "id": "Q6416448"
-    },
-    "iva": null,
-    "gracenote": null,
-    "rotten_tomatoes": null,
-    "facebook": null
-    }
-    }
-    ],
-    "updated": "2020-04-28T05:19:02+0100",
-    "term": "Parasite",
-    "status_code": 200,
-    "variant": "ivafull"
-    }
-    `);
-
-
 /*===============================================================
         Dependencies
 ===============================================================*/
@@ -233,113 +5,112 @@ const results = JSON.parse(`
 var axios = require("axios");
 var db = require("../models");
 
-
 /*===============================================================
         API Routes
 ===============================================================*/
 module.exports = function(app) {
+  // Gets all data for a given movie title
+  // calls OMDB Free api to ensure you are looking for a valid show.  Ends here if not valid (OMDB said no results)
+  // Check if data is in Movie Database, using the OMDB.data.Title
+  // call uTelly to get hosting information, and store in database for the next time it is called
+  // return all information needed.
+  app.get("/api/movies/:movie", async function(req, res) {
+    const movieName = req.params.movie;
+    var dbData;
 
-    // Gets all data for a given movie title
-        // calls OMDB Free api to ensure you are looking for a valid show.  Ends here if not valid (OMDB said no results)
-        // Check if data is in Movie Database, using the OMDB.data.Title
-        // call uTelly to get hosting information, and store in database for the next time it is called
-        // return all information needed.
-    app.get("/api/movies/:movie", async function(req, res) {
-        const movieName = req.params.movie;
-        var dbData;
-
-        // Pull information from OMDB
-        const omdb = await axios({
-            "method": "GET",
-            "url": "https://www.omdbapi.com/",
-            "params":{
-                "t":movieName,
-                "apikey":process.env.OMDBAPI
-            }
-        })
-
-        // If OMDB fails to find data, end the process.
-        if(omdb.data.Response ==='False'){
-            console.log(`${movieName} could was not found.`)
-            return res.send(omdb.data);
-        }
-
-        // Check local database to see if it has info
-        dbData = await db.Movies.findOne({
-            where:{
-                imdbID: omdb.data.imdbID
-            }
-        });
-
-        // if not there, make a call to uTelly for the tile.  Expect multiple results, and handle them all
-        if(dbData===null){
-            console.log("Data was not found.  Querying UTelly...");
-
-            // get data from UTELLY
-            const uTelly = await axios(
-                {
-                    "method":"GET",
-                    "url":"https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup",
-                    "headers":{
-                        "content-type":"application/octet-stream",
-                        "x-rapidapi-host":"utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-                        "x-rapidapi-key":process.env.UTELLYAPI,
-                    },
-                    "params":{
-                        "term":omdb.data.Title,
-                        "country":"us"
-                    }
-                }
-            )
-
-            //console.log(uTelly.data);
-
-            for (movie of uTelly.data.results){
-                //console.log(movie);
-                var newMovie = {
-                    name: movie.name,
-                    imdbID: movie.external_ids.imdb.id,
-            
-                    uTellyUpdated: uTelly.data.updated,
-                    uTellyID: movie.id,
-                    uTellyPicture: movie.picture,
-                    uTellyLocations: JSON.stringify(movie.locations)
-                }
-
-                // insert data into database, if it doesnt already exist
-                let thisID = await db.Movies.findOne({where:{imdbID: omdb.data.imdbID}}) != null
-                if(thisID != null){
-                    await db.Movies.create(newMovie);
-                }
-
-                // and store this movie for the return if it does actually match the IMDB id
-                if(newMovie.imdbID == omdb.data.imdbID){
-                    console.log(newMovie);
-                    dbData={};
-                    dbData.dataValues = newMovie;
-                }
-            }
-
-
-            // if STILL null after all of that, then UTelly has no idea what this content is, and we will create an empty listing so it doesnt cause issues in the future.
-            if(dbData===null){
-                await db.Movies.create({
-                    name:omdb.data.Title,
-                    imdbID:omdb.data.imdbID
-                });
-                dbData.dataValues={
-                    name:omdb.data.Title,
-                    imdbID:omdb.data.imdbID,
-                    uTellyUpdated: null,
-                    uTellyID: null,
-                    uTellyPicture: null,
-                    uTellyLocations: null
-                }
-            }
-        }
-
-        //We finall have all the data.  Return it.
-        res.json({...omdb.data, ...dbData.dataValues});
+    // Pull information from OMDB
+    const omdb = await axios({
+      method: "GET",
+      url: "https://www.omdbapi.com/",
+      params: {
+        t: movieName,
+        apikey: process.env.OMDBAPI
+      }
     });
 
-}
+    // If OMDB fails to find data, end the process.
+    if (omdb.data.Response === "False") {
+      console.log(`${movieName} could was not found.`);
+      return res.send(omdb.data);
+    }
+
+    // Check local database to see if it has info
+    dbData = await db.Movies.findOne({
+      where: {
+        imdbID: omdb.data.imdbID
+      }
+    });
+
+    // if not there, make a call to uTelly for the tile.  Expect multiple results, and handle them all
+    if (dbData === null) {
+      console.log("Data was not found.  Querying UTelly...");
+
+      // get data from UTELLY
+      const uTelly = await axios({
+        method: "GET",
+        url:
+          "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup",
+        headers: {
+          "content-type": "application/octet-stream",
+          "x-rapidapi-host":
+            "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+          "x-rapidapi-key": process.env.UTELLYAPI
+        },
+        params: {
+          term: omdb.data.Title,
+          country: "us"
+        }
+      });
+
+      //console.log(uTelly.data);
+
+      for (movie of uTelly.data.results) {
+        //console.log(movie);
+        var newMovie = {
+          name: movie.name,
+          imdbID: movie.external_ids.imdb.id,
+
+          uTellyUpdated: uTelly.data.updated,
+          uTellyID: movie.id,
+          uTellyPicture: movie.picture,
+          uTellyLocations: JSON.stringify(movie.locations)
+        };
+
+        // insert data into database, if it doesnt already exist
+        let thisID = await db.Movies.findOne({
+          where: { imdbID: omdb.data.imdbID }
+        });
+
+        if (thisID !== null) {
+          await db.Movies.create(newMovie);
+        }
+
+        // and store this movie for the return if it does actually match the IMDB id
+        if (newMovie.imdbID === omdb.data.imdbID) {
+          console.log(newMovie);
+          dbData = {};
+          dbData.dataValues = newMovie;
+        }
+      }
+
+      // if STILL null after all of that, then UTelly has no idea what this content is, and we will create an empty listing so it doesnt cause issues in the future.
+      if (dbData === null) {
+        await db.Movies.create({
+          name: omdb.data.Title,
+          imdbID: omdb.data.imdbID
+        });
+        dbData.dataValues = {
+          name: omdb.data.Title,
+          imdbID: omdb.data.imdbID,
+          uTellyUpdated: null,
+          uTellyID: null,
+          uTellyPicture: null,
+          uTellyLocations: null
+        };
+      }
+    }
+
+    //We finall have all the data.  Return it.
+    res.json({ ...omdb.data, ...dbData.dataValues });
+  });
+};
