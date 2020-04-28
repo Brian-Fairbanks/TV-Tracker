6a5fc9df6c01376096488a19dc3d4da2b821a424
Ryan Gautier <ryangautier2@gmail.com>
2020-04-28 15:17:10 -0500

    fixing for linter

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,18 +1,13 @@
 var isOpen = false;
-// if (!isOpen) {
-//     document.getElementById("menu-options").classList.add("hidden");
-//     document.getElementById("menu-options").classList.remove("block");
-// }
-document.getElementById("menu-btn").addEventListener("click", function(event) {
-    event.preventDefault();
-    isOpen = !isOpen;
-    if (isOpen) {
-        document.getElementById("menu-options").classList.add("my-block");
-        document.getElementById("menu-options").classList.remove("my-hidden");
-    }
-    else {
-        document.getElementById("menu-options").classList.remove("my-block");
-        document.getElementById("menu-options").classList.add("my-hidden");
-    }
-})
 
+document.getElementById("menu-btn").addEventListener("click", function(event) {
+  event.preventDefault();
+  isOpen = !isOpen;
+  if (isOpen) {
+    document.getElementById("menu-options").classList.add("my-block");
+    document.getElementById("menu-options").classList.remove("my-hidden");
+  } else {
+    document.getElementById("menu-options").classList.remove("my-block");
+    document.getElementById("menu-options").classList.add("my-hidden");
+  }
+});
