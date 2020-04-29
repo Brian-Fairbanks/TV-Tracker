61f8f586f061aa1347bee03c60070397e71d2dfd
Ryan Gautier <ryangautier2@gmail.com>
2020-04-29 14:37:53 -0500

    fixing index name

--- views/membersview.js
+++ views/membersview.js
@@ -1,5 +1,4 @@
-module.exports = function() {
-    return `<div>
+module.exports = `<div>
     <a href="/logout"
       class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-900 hover:bg-white mt-4 sm:mt-0">Logout</a>
     </div>
@@ -7,5 +6,4 @@ module.exports = function() {
     </nav>
     </header>
     <h2 class="text-center text-teal-500">Welcome <span class="member-name"></span></h2>
-  <script type="text/javascript" src="js/members.js"></script>`
-}
+  <script type="text/javascript" src="js/members.js"></script>`;
