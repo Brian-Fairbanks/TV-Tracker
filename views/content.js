17a40267e2738b6f9f9d143d56877ff2036eef27
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-04-30 09:30:01 -0500

    removed prettier, as it was clashing with indent rules on template strings.  Re-ran linter

--- views/content.js
+++ views/content.js
@@ -10,21 +10,21 @@ module.exports = function(props = {}, user) {
     <div class="w-full sm:w-1/2 lg:w-1/3 flex justify-center">
       <img class="m-2 w-1/2 sm:w-full h-auto" src="${props.Poster}" src="${
   props.Title
-}Poster">
+} Poster">
     </div>
     <div class="w-full sm:w-1/2 lg:w-2/3 px-3">
       <div class="flex flex-wrap w-full justify-between text-center font-bold">
         ${props.Ratings.map(rating => {
-          return `<div class="w-1/3">
+    return `<div class="w-1/3">
               <span>
               ${
-                rating.Source === "Internet Movie Database"
+  rating.Source === "Internet Movie Database"
     ? "IMDB"
     : rating.Source === "Rotten Tomatoes"
       ? "RT"
       : rating.Source === "Metacritic"
         ? "MC"
-                  : ""
+        : ""
 }
               </span>
               <span>${rating.Value}</span>
@@ -34,7 +34,7 @@ module.exports = function(props = {}, user) {
 
       <div class="pt-4">
         <span class="font-bold border border-black rounded py-0 px-2">${
-          props.Rated
+  props.Rated
 }</span>
         ${props.Genre}
         <br>
@@ -58,14 +58,14 @@ module.exports = function(props = {}, user) {
       <div class="w-full flex m-2 text-center pt-4">
         <div class="w-1/2">
           <div class="font-bold">Writers</div><ul>${props.Writer.split(",")
-            .map(writer => {
+    .map(writer => {
       return `<li>${writer}</li>`;
-            })
+    })
     .join("")}</ul>
         </div>
         <div class="w-1/2">
         <div class="font-bold">Actors</div> <ul>${props.Actors.split(",")
-          .map(actor => {
+    .map(actor => {
       return `<li>${actor}</li>`;
     })
     .join("")}</ul>
@@ -75,9 +75,9 @@ module.exports = function(props = {}, user) {
     </div>
     <div class="w-full flex justify-around mt-2 p-3 bg-gray-900">
       ${JSON.parse(props.uTellyLocations)
-        .map(location => {
+    .map(location => {
       return `<a href="${location.url}"><img class="logo" src="${location.icon}" src="${location.name}"></a>`;
-        })
+    })
     .join("")}
     </div>
   </div>
