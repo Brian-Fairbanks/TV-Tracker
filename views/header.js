f43ea30496f8d24cf0458aac2a5736c43f71b1ff
Ryan Gautier <ryangautier2@gmail.com>
2020-04-30 13:08:49 -0500

    append username to header

--- views/header.js
+++ views/header.js
@@ -32,20 +32,14 @@ module.exports = function (body, user) {
             </button>
           </div>
     
-          <div class="w-full flex-grow sm:flex sm:items-center sm:w-auto sm:block" id="menu-options">
-            <div class="text-sm sm:flex-grow">
-              <a href="#" class="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">
-                Docs
+          <div class="w-full flex-grow sm:flex sm:items-center sm:w-auto sm:block my-block" id="menu-options">
+            <div class="text-sm sm:flex-grow flex-row sm:justify-between flex mr-4">
+              <a href="/" class="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">
+                Home
               </a>
-              <a href="#" class="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">
-                Examples
-              </a>
-              <a href="#" class="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white">
-                Blog
-              </a>
-            </div>
             ${!user ?
-    `<div>
+    `</div>
+            <div>
               <a href="/login"
                 class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-900 hover:bg-white mt-4 sm:mt-0">Login</a>
               <a href="/signup"
@@ -54,7 +48,12 @@ module.exports = function (body, user) {
             </div>
           </div>
         </nav>
-      </header>` : `<div>
+      </header>` : `
+      <p class="text-teal-200 inline-block sm:mt-0 mt-4">Welcome ${user.email}!</p>
+      </div>
+      <div>
+      <a href="/user"
+        class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-900 hover:bg-white mt-4 sm:mt-0">My Watch List</a>
       <a href="/logout"
         class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-900 hover:bg-white mt-4 sm:mt-0">Logout</a>
       </div>
