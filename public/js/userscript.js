593abc4a49fb46a94acc794aa0824a8d1f45f1d2
Ryan Gautier <ryangautier2@gmail.com>
2020-04-29 21:23:55 -0500

    commenting out path in html routes

--- public/js/userscript.js
+++ public/js/userscript.js
@@ -1,21 +1,27 @@
 var moviesActive = false;
 document.getElementById("movie-hide").addEventListener("click", function() {
-    moviesActive = !moviesActive;
-    if (moviesActive) {
-        document.querySelectorAll(".movie").forEach(item => item.classList.add("hidden"));
-    }
-    else {
-        document.querySelectorAll(".movie").forEach(item => item.classList.remove("hidden"));
-    }
-})
+  moviesActive = !moviesActive;
+  if (moviesActive) {
+    document
+      .querySelectorAll(".movie")
+      .forEach(item => item.classList.add("hidden"));
+  } else {
+    document
+      .querySelectorAll(".movie")
+      .forEach(item => item.classList.remove("hidden"));
+  }
+});
 
 var showsActive = false;
 document.getElementById("show-hide").addEventListener("click", function() {
-    showsActive = !showsActive;
-    if (showsActive) {
-        document.querySelectorAll(".show").forEach(item => item.classList.add("hidden"));
-    }
-    else {
-        document.querySelectorAll(".show").forEach(item => item.classList.remove("hidden"));
-    }
-})
+  showsActive = !showsActive;
+  if (showsActive) {
+    document
+      .querySelectorAll(".show")
+      .forEach(item => item.classList.add("hidden"));
+  } else {
+    document
+      .querySelectorAll(".show")
+      .forEach(item => item.classList.remove("hidden"));
+  }
+});
