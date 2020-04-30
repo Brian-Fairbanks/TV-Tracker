87e49f3310ae89f178d93f8bc6f5b1ac61716791
Ryan Gautier <ryangautier2@gmail.com>
2020-04-29 21:18:06 -0500

    update views to be functions

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,27 +10,23 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      return res.redirect("/members");
+      return res.redirect("/user");
     }
     // res.sendFile(path.join(__dirname, "../public/signup.html"));
-    res.send(view.header(view.signup));
+    res.send(view.header(view.signup()));
   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      return res.redirect("/members");
+      return res.redirect("/user");
     }
     // res.sendFile(path.join(__dirname, "../public/login.html"));
-    res.send(view.header(view.login));
+    res.send(view.header(view.login()));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    // res.sendFile(path.join(__dirname, "../public/members.html"));
-    res.send(view.header(view.members));
-  });
 
   app.get("/user", isAuthenticated, function(req, res) {
     res.send(view.header(view.user()));
--- views/membersview.js
+++ views/membersview.js
@@ -1,4 +1,4 @@
-module.exports = `<div>
+module.exports = function() {`<div>
     <a href="/logout"
       class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-900 hover:bg-white mt-4 sm:mt-0">Logout</a>
     </div>
@@ -7,3 +7,4 @@ module.exports = `<div>
     </header>
     <h2 class="text-center text-teal-500">Welcome <span class="member-name"></span></h2>
   <script type="text/javascript" src="js/members.js"></script>`;
+};
