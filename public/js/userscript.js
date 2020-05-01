52cda3c93c76203300ae8fe6521afe5db4e48024
Ryan Gautier <ryangautier2@gmail.com>
2020-05-01 10:31:17 -0500

    update userpage styling

--- public/js/userscript.js
+++ public/js/userscript.js
@@ -27,24 +27,24 @@ document.getElementById("show-hide").addEventListener("click", function () {
 });
 
 $(document).ready(function () {
-  const watchlist = getWatchList();
+  // const watchlist = getWatchList();
 
 });
-function getWatchList() {
-  $.ajax({
-    url: "/api/user/watchlist",
-    type: "GET"
-  }).then(function (watchlist) {
-    watchlist.forEach(item => {
+// function getWatchList() {
+//   $.ajax({
+//     url: "/api/user/watchlist",
+//     type: "GET"
+//   }).then(function (watchlist) {
+//     watchlist.forEach(item => {
 
-      $("#movies").append(`<div class="w-full lg:max-w-4xl md:max-w-3xl sm:max-w-xl max-w-lg m-auto container flex-auto flex justify-around py-3 text-center items-center bg-gray-900 border-b-2 border-teal-500 text-white movie">
+//       $("#movies").append(`<div class="w-full lg:max-w-4xl md:max-w-3xl sm:max-w-xl max-w-lg m-auto container flex-auto flex justify-around py-3 text-center items-center bg-gray-900 border-b-2 border-teal-500 text-white movie">
 
-<h2 id="title"><img src="${item.}" alt="Movie poster"/>Movie Title</h2>
-<h2 id="genre">${item.}</h2>
-<h2 id="release-date">${item.}</h2>
-<h2 id="run-time">${item.}</h2>
-<h3 class="text-teal-200 hover:text-teal-500"><a href="/content/${movie.Title}-${movie.imdbID}">
-More info...</a></h3>
-</div>`);
-    })  });
-};
+// <h2 id="title"><img src="${item.}" alt="Movie poster"/>Movie Title</h2>
+// <h2 id="genre">${item.}</h2>
+// <h2 id="release-date">${item.}</h2>
+// <h2 id="run-time">${item.}</h2>
+// <h3 class="text-teal-200 hover:text-teal-500"><a href="/content/${movie.Title}-${movie.imdbID}">
+// More info...</a></h3>
+// </div>`);
+//     })  });
+// };
