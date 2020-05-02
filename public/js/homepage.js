18063aa55280dfc613a164542dce34767ff479cd
Ryan Gautier <ryangautier2@gmail.com>
2020-05-02 17:00:57 -0500

    add trending data to page, round corners

---
+++ public/js/homepage.js
@@ -0,0 +1,27 @@
+appenddata();
+
+function getTrending() {
+  return $.ajax({
+    url: "/api/user/trending",
+    type: "GET"
+  });
+}
+
+
+async function appenddata() {
+  const trending = await getTrending();
+
+  $("#trending").empty();
+  trending.forEach(item => {
+
+    var htmlstring = `<div class="w-1/3 mb-5">
+    <h2>
+    <a href="/content/${item.name}-${item.imdbID}">
+    <img src="${item.poster}" alt="${item.name}" style="width: 150px;" class="m-auto text-center">${item.name}
+    </a>
+    </h2>
+    </div>`;
+    $("#trending").append(htmlstring);
+
+  });
+}
