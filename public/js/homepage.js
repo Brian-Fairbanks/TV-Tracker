039602b07661e5ec9458b01bd0ab86b6024df4b8
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-05-02 21:09:10 -0500

    Fixed broken image links on main page

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -17,7 +17,9 @@ async function appenddata() {
     var htmlstring = `<div class="w-1/3 mb-5">
     <h2>
     <a href="/content/${item.name}-${item.imdbID}">
-    <img src="${item.poster}" alt="${item.name}" style="width: 150px;" class="m-auto text-center">${item.name}
+    <img src="${
+      (item.poster !== "N/A" && item.poster !==null)?item.poster:"/assets/poster1.png"
+    }" alt="${item.name}" style="width: 150px;" class="m-auto text-center">${item.name}
     </a>
     </h2>
     </div>`;
